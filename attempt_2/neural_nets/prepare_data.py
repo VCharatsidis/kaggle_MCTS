bdc1b65316fdf83b7a7efa6b5c9b47251805c039
Vasileios Charatsidis <charatsidisvasileios@gmail.com>
2024-12-05 12:25:45 +0100

    remove big files

--- attempt_2/neural_nets/prepare_data.py
+++
@@ -1,8 +0,0 @@
-import polars as pl
-
-test = pl.read_csv('../../um-game-playing-strength-of-mcts-variants/test.csv')
-print('Shape before dropping columns:', test.shape)
-
-train = pl.read_csv('../../um-game-playing-strength-of-mcts-variants/train.csv')
-print('Shape before dropping columns:', train.shape)
-
